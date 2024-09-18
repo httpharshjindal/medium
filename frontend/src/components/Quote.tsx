b5605e1a0ae3f63067863b916d7b92d558e1db81
Harsh Jindal <118842011+httpharshjindal@users.noreply.github.com>
2024-09-19 01:46:00 +0530

    Update Quote.tsx

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,6 +1,6 @@
 function Quote() {
   return (
-    <div className=" invisible sm:visible w-full h-full bg-zinc-50 flex flex-col px-5 justify-center items-start">
+    <div className=" invisible sm:visible w-full h-screen bg-zinc-50 flex flex-col px-5 justify-center items-start">
       <h1 className="text-xl font-bold sm:text-3xl">
         "The customer service i received was exceptional. The support team went
         above and beyond to address my concerns."
