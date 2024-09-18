4e6780707b22bb645d732ffcd697b038da3be432
Harsh Jindal <118842011+httpharshjindal@users.noreply.github.com>
2024-09-19 01:46:21 +0530

    Update Signup.tsx

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -11,7 +11,7 @@ function Signup() {
         showPublish={false}
         showAvatar={false}
       />
-      <div className="sm:flex w-full h-full justify-center items-center py-5">
+      <div className="sm:flex w-full h-full justify-center items-center">
         <Auth type="signup" />
         <Quote />
       </div>
